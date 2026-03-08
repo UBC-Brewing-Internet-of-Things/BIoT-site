d56345dba8e38befe78b4cd8d1b46583bd74544d
Sophie You <sophie.syou@hotmail.com>
2026-03-08 13:03:08 -0700

    Added subteams

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import SubteamHomepageCard from "@/src/components/SubteamHomepageCard";
 import HomepageImage from "@/src/images/home_images/beaker_home.jpg";
 import { subteamsDescription } from "@/src/data/homePageData";
 import styles from "@/src/styles/Homepage.module.css";
+import Logo from "@/src/images/logo_images/white_logo_text.png";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -9,8 +11,12 @@ export default function Page() {
     <div className={styles.parallax} style={{backgroundImage: `url(${HomepageImage.src})`}}>
       
       <div className={styles.home_title}>
-        <h1>What If Homebrewers Could Automate Brews Like Large Companies?</h1>
-        <p>Our team aims to design and implement low-cost data monitoring and develop innovative techniques to brew novel, high-quality beers through various projects.</p>
+        <Image
+        src={Logo}
+        alt="UBC BIoT Logo"
+        className={styles.home_logo}
+        />
+        <p>Known as “UBC’s beer-brewing design team”, Biological Internet of Things (BIoT) has expanded to include a variety of projects in process control, R&D, and automation, including building a miniature chemical plant for the ChemE Cube competition, IoT process automation devices, and experimental research projects in food technology like making glow-in-the-dark beer.</p>
       </div>
     </div>
     <h1>Meet the Subteams</h1>
--- src/images/home_images/subteam_images/index.ts
+++ src/images/home_images/subteam_images/index.ts
@@ -1,7 +1,8 @@
 import brew from "./brew.png";
 import instrumentation from "./instrumentation.png";
 import lab from "./lab.png";
+import chemEcube from "./chem_e_cube.jpg";
 
 export const subteamImages = {
-    brew,instrumentation, lab
+    brew, instrumentation, lab, chemEcube
 };
