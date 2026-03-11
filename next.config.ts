314aa3fa01f0a3cfebf6ac9ad5c33f4633195f3f
Sophie You <sophie.syou@hotmail.com>
2026-03-10 21:34:42 -0700

    Attempt to deploy website

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
+  basePath: "/bioT-site",  
+  assetPrefix: "/bioT-site",
 };
 
 export default nextConfig;
