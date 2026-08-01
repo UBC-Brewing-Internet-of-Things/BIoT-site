4b34030f6a2b636a0b8d9a7fd87b4836ca71124f
Sophie You <sophie.syou@hotmail.com>
2026-08-01 12:38:53 -0700

    Added Basic Project Article template

--- app/biofluorescentBeer/page.tsx
+++ app/biofluorescentBeer/page.tsx
@@ -1,4 +1,5 @@
 import BasicArticle from "@/src/components/BasicArticle";
+import Biofluorescent_Beer from "../../src/images/project_images/glow_in_the_darkbeer_project.jpg";
 
 export default function page() {
     return(
@@ -8,9 +9,9 @@ export default function page() {
             description="A project that aims to create a biofluorescent beer using genetically modified yeast."
             date="2023-2024"
             team={["Lab Team", "Brew Team"]}
-            image1="/images/biofluorescentBeer.jpg"
-            image2 = "/images/biofluorescentBeer.jpg"
-            image3 = "/images/biofuorescentBeer.jpg" />
+            image1={Biofluorescent_Beer}
+            image2 = {Biofluorescent_Beer}
+            image3 = {Biofluorescent_Beer} />
         </div>
     );
 
--- src/components/BasicArticle.tsx
+++ src/components/BasicArticle.tsx
@@ -1,36 +1,37 @@
 import Image from 'next/image';
 import styles from '../styles/BasicArticle.module.css';
 
-function BasicArticle({title, summary, description, date, team, image1, image2, image3}: {title: string, summary: string, description: string, date: string, team: string[], image1: string, image2: string, image3: string}) {
+function BasicArticle({title, summary, description, date, team, image1, image2, image3}: {title: string, summary: string, description: string, date: string, team: string[], image1: any, image2: any, image3: any}) {
     return (
         <div className={styles.container}>
             <div>
                 <div>
                     <h2 className={styles.title}>{title}</h2>
-                    <p><i>{date}</i>, {team.join(', ')}</p>
+                    <p className = {styles.info}><i>{date}</i>, {team.join(', ')}</p>
                 </div>
-                <p>{summary}</p>
             </div>
+            <div>
+                <Image 
+                src={image1} 
+                alt={title}
+                height={300} 
+                width={300} />
+                <div>
+                    <Image
+                    src={image2} 
+                    alt={title}
+                    height={300} 
+                    width={300}/>
+                    <Image
+                    src={image3} 
+                    alt={title}
+                    height={300} 
+                    width={300}/>
+                </div>
+            </div>
+            
             <p>{description}</p>
             
-                
-         
-            
-            <Image 
-            src={image1} 
-            alt={title}
-            height={300} 
-            width={300} />
-            <Image
-            src={image2} 
-            alt={title}
-            height={300} 
-            width={300}/>
-            <Image
-            src={image3} 
-            alt={title}
-            height={300} 
-            width={300}/>
         </div>
     );
 }
