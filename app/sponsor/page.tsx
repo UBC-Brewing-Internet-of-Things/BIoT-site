2135ba8e5df08fe9a9df86f1346803e4baa7e308
Sophie You <sophie.syou@hotmail.com>
2026-08-28 18:43:21 -0700

    Changed Sponsor Page styles and added ChemE Cube subteams

--- app/sponsor/page.tsx
+++ app/sponsor/page.tsx
@@ -4,20 +4,25 @@ import Link from 'next/link';
 import sponsorshipPackage from '../../src/images/sponsor_images/BIoT_Sponsorship_Package_2526.png';
 import SponsorCard from '@/src/components/SponsorCard';
 import {sponsors} from '@/src/data/sponsorsData';
+import Arrows from '../../src/images/logo_images/three_right_arrows.png';
 
 export default function page() {
   return (
-    <main className={styles.page}>
-      <h1>Join Us In Shaping A Greener Future In Homebrewing!</h1>
-      <h2>Sponsor The Team. Get Benefits.</h2>
+    <main >
+      <div className = {styles.header}>
+        <Image src={Arrows} alt="right arrows" className={styles.arrows} />
+        <h1>Sponsor The Team. <br/> 
+        Get Benefits.</h1>
+      </div>
+      <div className={styles.page}>
       <p>Sponsoring UBC Biological Internet of Things means more than supporting an organization; it means investing in the next generation of engineers and innovators. Our team is dedicated to equipping our members with hands-on experience in process engineering, biotechnology, control systems, Internet of Things devices, technical presentations, implementing sustainable practice, and much more. 
         <br /> <br />
       By sponsoring BIoT, you not only contribute to the education and development of aspiring engineers but also support the advancement of innovations in these fields. Join us as we strive to make a positive impact on both our industry and our planet.</p>
       {/*Sponsorship Package*/}
+      <h3 >Sponsorship Package</h3>
       <div className={styles.info}>
         <div className={styles.sponsorPackageText}>
-          <h2 style={{textAlign: 'left'}}>Sponsorship Package</h2>
-          <p>Please check out our sponsorship package for more info!</p>
+          
         </div>
         <Link href="https://drive.google.com/file/d/1Be9r7AWDOYDRoCpGzTZTHKS2uMpgLdT_/view">
         <Image src={sponsorshipPackage} alt="Sponsorship Package" className={styles.image} />
@@ -42,6 +47,7 @@ export default function page() {
         {sponsors.Supporters.map((sponsor, index) => (
           <SponsorCard key={index} name={sponsor.title} image={sponsor.logo} link={sponsor.link}/>
         ))}
+      </div>
       </div>
     </main>
   );
--- src/images/home_images/subteam_images/index.ts
+++ src/images/home_images/subteam_images/index.ts
@@ -2,7 +2,8 @@ import brew from "./brew.png";
 import instrumentation from "./instrumentation.png";
 import lab from "./lab.png";
 import chemEcube from "./chem_e_cube.jpg";
+import chemEcubeCad from "./cheme_cube_cad.png";
 
 export const subteamImages = {
-    brew, instrumentation, lab, chemEcube
+    brew, instrumentation, lab, chemEcube, chemEcubeCad
 };
