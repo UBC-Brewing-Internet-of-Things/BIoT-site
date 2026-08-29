bd12af97caea611897d9ffcc0b15b1f8aec409b0
Sophie You <sophie.syou@hotmail.com>
2026-08-28 17:38:22 -0700

    Changed homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import SubteamHomepageCard from "@/src/components/SubteamHomepageCard";
-import HomepageImage from "@/src/images/home_images/home1.jpg";
 import { subteamsDescription } from "@/src/data/homePageData";
 import styles from "@/src/styles/Homepage.module.css";
-import Logo from "@/src/images/logo_images/white_logo_text.png";
+import Logo from "@/src/images/logo_images/colourful_logo_text.png";
 import Image from "next/image";
 import Biofluorescent_Beer from "../src/images/project_images/glow_in_the_darkbeer_project.jpg";
 import Link from "next/link";
 import CurrentProjectCard from "@/src/components/CurrentProjectCard";
 import ChemE_Cube from "../src/images/project_images/cheme_cube.jpg";
+import RightArrows from "../src/images/logo_images/right_arrows.png";
+import LeftArrows from "../src/images/logo_images/left_arrows.png";
+import Leaf from "../src/images/logo_images/half_leaf.png";
 
 export default function Page() {
   const beerRnD = subteamsDescription.slice(0, 3);
@@ -17,18 +19,35 @@ export default function Page() {
   return (
   <main className={styles.container}>
     {/*Main Section*/}
-    <div className={styles.parallax} style={{backgroundImage: `url(${HomepageImage.src})`}}>
+    <div className={styles.titlebackground} >
       
       <div className={styles.home_title}>
+        <Image
+        src={RightArrows}
+        alt="right arrows"
+        className={styles.arrows}
+        />
         <Image
         src={Logo}
         alt="UBC BIoT"
         className={styles.home_logo}
         />
-        <p>Known as “UBC’s beer-brewing design team”, Biological Internet of Things (BIoT) has expanded to include a variety of projects in process control, R&D, and automation, including building a miniature chemical plant for the ChemE Cube competition, IoT process automation devices, and experimental research projects in food technology like making glow-in-the-dark beer.</p>
+        <Image
+        src={LeftArrows}
+        alt="left arrows"
+        className={styles.arrows}
+        />
       </div>
+      <div className = {styles.rectangle_text}>
+          <p>Known as “UBC’s beer-brewing design team”, Biological Internet of Things (BIoT) has expanded to include a variety of projects in process control, R&D, and automation, including building a miniature chemical plant for the ChemE Cube competition, IoT process automation devices, and experimental research projects in food technology like making glow-in-the-dark beer.</p>
+        </div>
     </div>
     {/*Current Project Section*/}
+    <Image
+        src={Leaf}
+        alt="leaf"
+        className={styles.leaf}
+        />
     <div className = {styles.projectContainer}>
       <h1>Current Projects</h1>
       <div className = {styles.projectsButtonContainer}>
